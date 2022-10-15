e6684c91cca154c3cff91fa027aaf4b66cae66ad
angganova2019 <angga24.opa@gmail.com>
2022-10-15 12:09:21 +0700

    firs commit

---
+++ routes.js
@@ -0,0 +1,65 @@
+const { getAll, getOne, createUser, updateUser, deleteUser } = require("./activity");
+const { getAllTodo, getOneTodo, createTodo, deleteTodo, updateTodo } = require("./todo");
+
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: () => {
+            return `Hello world!`
+        },
+    },
+    {
+        method: 'GET',
+        path: '/activity-groups',
+        handler: getAll,
+    },
+    {
+        method: 'GET',
+        path: '/activity-groups/{id}',
+        handler: getOne,
+    },
+    {
+        method: 'POST',
+        path: '/activity-groups',
+        handler: createUser,
+    },
+    {
+        method: 'PATCH',
+        path: '/activity-groups/{id}',
+        handler: updateUser,
+    },
+    {
+        method: 'DELETE',
+        path: '/activity-groups/{id}',
+        handler: deleteUser,
+    },
+    {
+        method: 'GET',
+        path: '/todo-items',
+        handler: getAllTodo,
+    },
+    {
+        method: 'GET',
+        path: '/todo-items/{id}',
+        handler: getOneTodo,
+    },
+    {
+        method: 'POST',
+        path: '/todo-items',
+        handler: createTodo,
+    },
+    {
+        method: 'PATCH',
+        path: '/todo-items/{id}',
+        handler: updateTodo,
+    },
+    {
+        method: 'DELETE',
+        path: '/todo-items/{id}',
+        handler: deleteTodo,
+    },
+];
+
+module.exports = routes;
